4b352e398a5bb29102e309cf157925feeeecaa12
Jeferson sena <jefinho368@gmail.com>
2023-07-05 10:27:31 -0300

    Create Startup.cs and refactor Progam.cs

--- Geek_Shopping/GeekShopping.ProductApi/Model/Base/BaseEntity.cs
+++ Geek_Shopping/GeekShopping.ProductApi/Model/Base/BaseEntity.cs
@@ -1,6 +1,12 @@
-﻿namespace GeekShopping.ProductApi.Model.Base
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.ProductApi.Model.Base
 {
     public class BaseEntity
     {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
     }
 }
---
+++ Geek_Shopping/GeekShopping.ProductApi/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace GeekShopping.ProductApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+       
+
+   
+    }
+}
