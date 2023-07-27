6158ea776fcf0ff8835c8154ba41f54398cfeac4
Jeferson sena <jefinho368@gmail.com>
2023-07-27 20:26:46 -0300

    Create ProductModel in GeekShopping.Web and create ServicesUrl in appsettings.json

---
+++ Geek_Shopping/GeekShopping.web/Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GeekShopping.web.Models
+{
+    public class ProductModel
+    {
+        public long id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public String Description { get; set; }
+        public string CategoryName { get; set; }
+        public string imageURL { get; set; }
+    }
+}
