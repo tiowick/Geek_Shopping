60d9d62833fcb5c65c09689e7e86608d0d848b70
Jeferson sena <jefinho368@gmail.com>
2023-07-27 20:34:40 -0300

    Create IProductServices

---
+++ Geek_Shopping/GeekShopping.web/Services/IServices/IProductInterface.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.web.Models;
+
+namespace GeekShopping.web.Services.IServices
+{
+    public interface IProductInterface
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> ProductById(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task <bool>DeleteProduct(long id);
+    }
+}
