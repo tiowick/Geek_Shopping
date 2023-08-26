6cd8ecd92cc03976de1ec9183de9088700487ff2
Jeferson sena <jefinho368@gmail.com>
2023-08-26 10:18:16 -0300

    Refactor IdentityConfiguration.cs

--- Geek_Shopping/GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
+++ Geek_Shopping/GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace GeekShopping.IdentityServer.Configuration
 {
@@ -36,11 +37,29 @@ namespace GeekShopping.IdentityServer.Configuration
                     ClientSecrets = {new Secret("my_super_secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     AllowedScopes = {"read", "write", "profile"}
-                
-                }
-                
-            
-            
+
+                },
+
+                new Client
+                {
+
+                    ClientId = "geek_shopping",
+                    ClientSecrets = {new Secret("my_super_secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = {"http://localhost:57914/signin-oidc"},
+                    PostLogoutRedirectUris = {"http://localhost:57914/signout-callback-oidc"},
+                    AllowedScopes = new List<String>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "geek_shopping"
+                    }
+
+                },
+
+
+
             };
     }
 
