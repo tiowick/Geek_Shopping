f16991d50356c0fbb84a9877fd954cb1b3258502
Jeferson sena <jefinho368@gmail.com>
2023-09-01 18:18:53 -0300

    Create Class Startup in GeekShopping.CartApi and configure

---
+++ Geek_Shopping/GeekShopping.CartApi/Startup.cs
@@ -0,0 +1,39 @@
+﻿namespace GeekShopping.CartApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+        }
+    }
+}
+        
+
+
