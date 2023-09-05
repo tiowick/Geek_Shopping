ba79344b04286c4393027dca21f13120189777fc
Jeferson sena <jefinho368@gmail.com>
2023-09-05 09:08:37 -0300

    Starting to Implement the Repository Classes

---
+++ Geek_Shopping/GeekShopping.CartApi/Repository/CartRepository.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using GeekShopping.CartApi.Data.ValueObjects;
+using GeekShopping.CartApi.Model.Context;
+
+namespace GeekShopping.CartApi.Repository
+{
+    public class CartRepository : ICartRepostory
+    {
+        private readonly MySQLContext _context;
+        private IMapper _mapper;
+
+        public CartRepository(MySQLContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+
+        }
+        public Task<bool> ApplyCupom(string userId, string cupomCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ClearCart(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartDTO> FindCartByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveCupom(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFromCart(long cartDetailsId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartDTO> SaveOrUpdateCart(CartDTO cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Geek_Shopping/GeekShopping.CartApi/Repository/ICartRepostory.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.CartApi.Data.ValueObjects;
+
+namespace GeekShopping.CartApi.Repository
+{
+    public interface ICartRepostory
+    {
+        Task<CartDTO> FindCartByUserId (string userId);
+        Task<CartDTO> SaveOrUpdateCart(CartDTO cart);
+        Task<bool> RemoveFromCart(long cartDetailsId);
+        Task<bool> ApplyCupom(string userId, string cupomCode);
+        Task<bool> RemoveCupom(string userId);
+        Task<bool> ClearCart(string userId);
+    }
+}
